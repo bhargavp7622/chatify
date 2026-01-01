31de4ca081098ee168bbd77bcbc51da08b1b75a5
bhargavp7622 <112881835+bhargavp7622@users.noreply.github.com>
2026-01-01 11:31:24 +0530

    Packages install and setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,24 @@
 import express from 'express';
+import path from 'path';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 
 const app = express();
 
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
