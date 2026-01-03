423b61cb88374fa2ce72f94498c8cb2d1d4571bf
bhargavp7622 <112881835+bhargavp7622@users.noreply.github.com>
2026-01-03 17:26:30 +0530

    Sign up code env setup

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-    try{
-     const connect =  await mongoose.connect(process.env.MONGO_URL);
-     console.log('mongo db connected successfully', connect.connection.host);
-    }catch(err){
+    try {
+        const { MONGO_URL } = process.env;
+        if (!MONGO_URL) throw new Error(" MONFO_URL is not set")
+        const connect = await mongoose.connect(process.env.MONGO_URL);
+        console.log('mongo db connected successfully', connect.connection.host);
+    } catch (err) {
         console.error('Error connect mongodb', err);
         process.exit(1);
     }
