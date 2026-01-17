4dcf798d1da97a518d2ec03d5bf3ef9cb6f8da64
bhargavp7622 <112881835+bhargavp7622@users.noreply.github.com>
2026-01-17 11:38:32 +0530

    Update profile pic api

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import { generateToken } from '../lib/utils.js';
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
 import { ENV } from '../lib/env.js';
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup = async (req, res) => {
 
@@ -48,12 +49,12 @@ export const signup = async (req, res) => {
             });
 
             // send a welcome email
-            try{
+            try {
                 await sendWelcomeEmail(saveUser.email, saveUser.fullName, ENV.CLIENT_URL);
-            }catch(error){
+            } catch (error) {
 
             }
-        }else{
+        } else {
             res.status(400).json({ message: "Invalid user data" });
         }
 
@@ -70,15 +71,15 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
     const { email, password } = req.body;
 
-    if(!email || !password){
-        return res.status(400).json({ message: "Email and Password are required!"});
+    if (!email || !password) {
+        return res.status(400).json({ message: "Email and Password are required!" });
     }
-    try{    
-        const user = await User.findOne({email});
-        if(!user) return res.status(400).json({ message: "Invalid credentials"});
+    try {
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ message: "Invalid credentials" });
 
         const isPasswordCorrect = await bcrypt.compare(password, user.password);
-        if(!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials"});
+        if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid credentials" });
 
         generateToken(user._id, res)
 
@@ -88,12 +89,34 @@ export const login = async (req, res) => {
             email: user.email,
             profilePic: user.profilePic
         })
-    }catch(error){
-        res.status(500).json({ message: "Internal server error"});
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
     }
 }
 
 export const logout = async (_, res) => {
-    res.cookie("jwt", "", {maxAge: 0})
-    res.status(200).json({ message: "Logged out successfully"});
+    res.cookie("jwt", "", { maxAge: 0 })
+    res.status(200).json({ message: "Logged out successfully" });
 }
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { profilePic } = req.body;
+    if (!profilePic) return res.status(400).json({ message: "Profile pic is required" });
+
+    const userId = req.user._id;
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { profilePic: uploadResponse.secure_url },
+      { new: true }
+    );
+
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log("Error in update profile:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import { connectDB } from './lib/db.js';
@@ -15,6 +16,7 @@ const PORT = ENV.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
